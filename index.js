8f4bd60bca2a577c8bcf5bfb886d93960231ac27
Ayrat <gimranov45@gmail.com>
2022-03-11 20:27:59 -0500

    added get resuqest

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const { application } = require('express');
 const express = require('express');
 const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3001;
 
+
+
 const server = express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+server.get('/', (req, res)=> {
+  res.status(200).send('I like pizza!')
+})
+
+
 const io = socketIO(server);
+
+
 
 io.on('connection', (socket) => {
   console.log('Client connected with ad ID:', socket.id);
